1d7c9672605a55446815691f73073d891d390c6b
Yusuf <baliyusuf675@gmail.com>
2023-08-29 22:02:10 +0300

    v4

--- app/page.js
+++ app/page.js
@@ -1,3 +1,16 @@
-export default function Home() {
-  return <div>Hello Next.js</div>;
-}
+const Link = "next/link";
+
+<div className="relative h-screen w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
+  <div className="bg-black w-full h-full bg-opacity-50">
+    <div className="text-white text-center relative top-2/4 m-auto">
+      <h1 className="text-5xl font-[900] text-center mb-3">
+        Unlimited movies, TV shows, and more
+      </h1>
+      <p className="text-2xl font-[400] ">Watch anywhere. Cancel anytime.</p>
+      {/* //? next/link arka planda sayfayı önceden fetch edilir. Bu, client tarafı gezintilerin performansını iyileştirmek için kullanışlıdır. Görünüm alanındaki herhangi bir <Link />  önceden yüklenecektir. */}
+      <Link href="/register">
+        <button className="btn-danger w-[250px]">Get Started</button>
+      </Link>
+    </div>
+  </div>
+</div>;
