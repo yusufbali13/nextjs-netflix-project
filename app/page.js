f4bdf823b7e5830a5a49daf0fb51bad289836cdb
Yusuf <baliyusuf675@gmail.com>
2023-08-31 23:16:00 +0300

    end

--- app/page.js
+++ app/page.js
@@ -1,9 +1,20 @@
+"use client";
+import { useAuthContext } from "@/context/AuthContext";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 //? app folder içindeki page.js "/" route olan home sayfasıdır
 
 //? nextjs'de tüm componentler default olarak server componenttir. client component kullanmak için "use-client" direktifini kullanmamız gerekir
 
 export default function Home() {
+  const { currentUser } = useAuthContext();
+  let router = useRouter();
+
+  useEffect(() => {
+    currentUser && router.push("/movies");
+  }, [currentUser]);
+
   return (
     <div className="relative h-screen w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
       <div className="bg-black w-full h-full bg-opacity-50">
