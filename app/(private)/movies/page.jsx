516e0c5e742275af2d3d3650c8b738e56e3be42c
Yusuf <baliyusuf675@gmail.com>
2023-08-31 22:44:06 +0300

    HeroSection

--- app/(private)/movies/page.jsx
+++ app/(private)/movies/page.jsx
@@ -1,6 +1,7 @@
 import { getMovies } from "@/helpers/movieFunctions";
 import React from "react";
 import MovieSection from "./components/MovieSection";
+import HeroSection from "./components/HeroSection";
 
 export const metadata = {
   title: "Movies",
@@ -10,6 +11,11 @@ const Movies = async () => {
   console.log(movies);
   return (
     <>
+      <HeroSection
+        title={movies[0]?.title}
+        overview={movies[0]?.overview}
+        id={movies[0]?.id}
+      />
       <div className="px-4 md:px-12 mt-4">
         <MovieSection title="NOW PLAYING" type="now-playing" />
         <MovieSection title="POPULAR" type="popular" />
